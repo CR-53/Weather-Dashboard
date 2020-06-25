f21aeefb9cb3e46184ef1b289224be44a602d43a
Chris Roschi <chrisroschi53@gmail.com>
2020-06-25 11:47:01 +0930

    added icon, temp and windspeed

--- script.js
+++ script.js
@@ -24,13 +24,14 @@ $("#searchButton").on("click", function () {
     // Adds the list item to the list
     $("#history").append(historyItem);
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + APIkey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + APIkey + "&units=metric";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (result) {
         console.log(result);
+        console.log(result.wind.speed);
         
         // Uses the unix timestamp and converts into readable date format
         var UNIX_Timestamp = result.dt;
@@ -39,12 +40,25 @@ $("#searchButton").on("click", function () {
         var month = date.getMonth() + 1;
         var year = date.getFullYear();
         var cityDate = "(" + day + "/" + month + "/" + year + ")";
-        console.log(cityDate);
 
-
+        // Creates a heading featuring the city name + date
         weatherCity = $("<h3>");
         weatherCity.text(result.name + " " + cityDate);
+
+        // Fetches the image icon from the api and displays it in an <img> element
+        var icon = $("<img>");
+        var iconCode = result.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        icon.attr("src", iconURL);
+
+        var temp = $("<h5>");
+        // Included "&units=metric" in the queryURL, so we will use degerees celsius
+        temp.text(result.main.temp + " °C");
+
+        var windSpeed = $("<h5>");
+        // Convert wind speed from m/s to km/h
+        windSpeed.text((result.wind.speed * 3.6) + " km/h");
         
-        $("#weather-results").append(weatherCity);
+        $("#weather-results").append(weatherCity, icon, temp, windSpeed);
     });
 });
